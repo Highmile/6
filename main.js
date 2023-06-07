1e1d01d21d10df5fbe0c823859acc20869d30ae3
Laziz <Saidkulov13@gmail.com>
2023-06-07 06:37:28 -0700

    new commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function request(fname, age) {
+    alert(`Salom ${fname} yoshingiz ${age} da`)
+}
+
+let userName = prompt("Ismingizini kriting")
+let userAge = +prompt("Yoshingizni kriting")
+
+request(userName, userAge)
